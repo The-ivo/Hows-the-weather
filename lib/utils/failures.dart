708e2e2b4a6f906a9ba2d9881c738217e75aed9d
l.ivo <lucas.ivo@shockit.com.br>
2025-05-24 12:21:39 -0300

    feat(failures): create failures class for http requests

---
+++ lib/utils/failures.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  const Failure({
+    this.code,
+    this.message,
+  });
+
+  final String? code;
+  final String? message;
+
+  @override
+  List<Object?> get props => [
+        code,
+        message,
+      ];
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure({super.code, super.message});
+}
