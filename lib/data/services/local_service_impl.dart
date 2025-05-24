fd425d19a47b64eb1609920a08601a3dab15caf4
livo <lucas.ivo@shockit.com.br>
2025-05-24 16:47:54 -0300

    fix(local local service impl): fix class name

--- lib/data/services/local_service_impl.dart
+++ lib/data/services/local_service_impl.dart
@@ -1,7 +1,7 @@
 import 'package:hows_the_weather/data/services/interfaces/local_service.dart';
 
-class LocalSeverviceImpl extends LocalService {
-  LocalSeverviceImpl({required super.sharedPreferences});
+class LocalServiceImpl extends LocalService {
+  LocalServiceImpl({required super.sharedPreferences});
 
   @override
   Future<bool> saveUserToken(String token) async {
