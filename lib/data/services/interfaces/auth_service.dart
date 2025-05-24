42864f6633dde8895becb11abb26be366dd2576d
livo <lucas.ivo@shockit.com.br>
2025-05-24 16:13:05 -0300

    fix(auth service): change body for query params on request

--- lib/data/services/interfaces/auth_service.dart
+++ lib/data/services/interfaces/auth_service.dart
@@ -2,9 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:hows_the_weather/data/model/auth_model.dart';
 
 abstract class AuthService {
-  final Dio dioClient;
+  const AuthService({required this.dioClient});
 
-  AuthService({required this.dioClient});
+  final Dio dioClient;
 
   Future<AuthModel> login({
     required String email,
