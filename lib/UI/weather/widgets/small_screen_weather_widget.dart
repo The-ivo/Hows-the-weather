b44137ce0a2ae89e23352edcb35c3698baa27b2a
livo <lucas.ivo@shockit.com.br>
2025-05-25 11:28:49 -0300

    wip(small screen weather widget): keep implementing small screen weather widget

--- lib/UI/weather/widgets/small_screen_weather_widget.dart
+++ lib/UI/weather/widgets/small_screen_weather_widget.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hows_the_weather/UI/core/theme/typography/typography.dart';
+import 'package:hows_the_weather/UI/weather/blocs/get_weather_bloc/get_weather_bloc.dart';
 
 class SmallScreenWeatherWidget extends StatelessWidget {
   const SmallScreenWeatherWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final weather = context.read<GetWeatherBloc>().state.weatherModel;
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'How\'s the weather?',
+              style: WeatherAppTypo.of(context)?.displayXsm?.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              weather!.name,
+              style: WeatherAppTypo.of(context)?.displaySm?.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+            ),
+            SizedBox(height: 10),
+            SizedBox(height: 10),
+            Text(
+              'Now: ${weather.main.temp}°F',
+              style: WeatherAppTypo.of(context)?.display2xsm?.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
