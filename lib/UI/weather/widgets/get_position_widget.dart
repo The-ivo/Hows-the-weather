d2b05cfc1cc4ca527ad25bb3a1a01a256b9cf1e4
livo <lucas.ivo@shockit.com.br>
2025-05-25 01:57:14 -0300

    refactor(get position widget): implement widget to get user's position

---
+++ lib/UI/weather/widgets/get_position_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hows_the_weather/UI/core/theme/typography/typography.dart';
+import 'package:hows_the_weather/UI/weather/blocs/get_position_bloc/get_position_bloc.dart';
+import 'package:hows_the_weather/utils/enums.dart';
+
+class GetPositionWidget extends StatelessWidget {
+  const GetPositionWidget({super.key, required this.weather});
+
+  final Widget weather;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GetPositionBloc, GetPositionState>(
+      builder: (context, getPositionState) {
+        if (getPositionState.status == GetCurrentPositionStatus.failure) {
+          return Center(
+            child: Padding(
+              padding: EdgeInsets.all(35),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/11d@2x.png'),
+                  Text(
+                    'We are unable to proceed because:',
+                    style: WeatherAppTypo.of(context)?.displayMd?.copyWith(
+                          color: Colors.white,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    getPositionState.error ?? '',
+                    style: WeatherAppTypo.of(context)?.displaySm?.copyWith(
+                          color: Colors.white,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else if (getPositionState.status == GetCurrentPositionStatus.success) {
+          return weather;
+        }
+        return Center(
+          child: Padding(
+            padding: EdgeInsets.all(35),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'We need your location to continue',
+                  style: WeatherAppTypo.of(context)?.displayLg?.copyWith(
+                        color: Colors.white,
+                      ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 32),
+                CircularProgressIndicator(),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
