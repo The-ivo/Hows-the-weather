3f82fab9021e2b11fe69c70fc447fd48b4b065b2
livo <lucas.ivo@shockit.com.br>
2025-05-25 01:57:59 -0300

    WIP(large screen weather): keep implementing widget to show weather data on large screen

---
+++ lib/UI/weather/widgets/large_screen_weather_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LargeScreenWeatherWidget extends StatelessWidget {
+  const LargeScreenWeatherWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
