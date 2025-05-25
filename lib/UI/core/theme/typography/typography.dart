ee5d65e63eda42fa711ebed4a5a6f76fe9336d08
livo <lucas.ivo@shockit.com.br>
2025-05-25 01:50:50 -0300

    feature(typography): Implement text style themes and font

---
+++ lib/UI/core/theme/typography/typography.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:hows_the_weather/UI/core/theme/typography/typography_text_styles.dart';
+
+class WeatherAppTypo extends ThemeExtension<WeatherAppTypo> {
+  WeatherAppTypo({
+    this.display2xl = AppTextStyles.display2xl,
+    this.displayXl = AppTextStyles.displayXl,
+    this.displayLg = AppTextStyles.displayLg,
+    this.displayMd = AppTextStyles.displayMd,
+    this.displaySm = AppTextStyles.displaySm,
+    this.displayButton = AppTextStyles.displayButton,
+  });
+
+  final TextStyle? display2xl;
+  final TextStyle? displayXl;
+  final TextStyle? displayLg;
+  final TextStyle? displayMd;
+  final TextStyle? displaySm;
+  final TextStyle? displayButton;
+
+  @override
+  ThemeExtension<WeatherAppTypo> copyWith({
+    TextStyle? display2XL,
+    TextStyle? displayXL,
+    TextStyle? displayLG,
+    TextStyle? displayMD,
+    TextStyle? displaySM,
+    TextStyle? displayButton,
+  }) {
+    return WeatherAppTypo(
+      display2xl: display2XL ?? display2xl,
+      displayXl: displayXL ?? displayXl,
+      displayLg: displayLG ?? displayLg,
+      displayMd: displayMD ?? displayMd,
+      displaySm: displaySM ?? displaySm,
+      displayButton: displayButton ?? displayButton,
+    );
+  }
+
+  @override
+  ThemeExtension<WeatherAppTypo> lerp(
+    covariant ThemeExtension<WeatherAppTypo>? other,
+    double t,
+  ) {
+    if (other is! WeatherAppTypo) {
+      return this;
+    }
+
+    return WeatherAppTypo(
+      display2xl: TextStyle.lerp(display2xl, other.display2xl, t),
+      displayXl: TextStyle.lerp(displayXl, other.displayXl, t),
+      displayLg: TextStyle.lerp(displayLg, other.displayLg, t),
+      displayMd: TextStyle.lerp(displayMd, other.displayMd, t),
+      displaySm: TextStyle.lerp(displaySm, other.displaySm, t),
+      displayButton: TextStyle.lerp(displayButton, other.displayButton, t),
+    );
+  }
+
+  static WeatherAppTypo? of(BuildContext context) {
+    return Theme.of(context).extension<WeatherAppTypo>();
+  }
+}
