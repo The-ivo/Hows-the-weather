6a11d96ac508d653dab2e294bee1eeb442500afa
livo <lucas.ivo@shockit.com.br>
2025-05-25 01:58:11 -0300

    WIP(small screen weather): keep implementing widget to show weather data on small screen

---
+++ lib/UI/weather/widgets/small_screen_weather_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SmallScreenWeatherWidget extends StatelessWidget {
+  const SmallScreenWeatherWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
