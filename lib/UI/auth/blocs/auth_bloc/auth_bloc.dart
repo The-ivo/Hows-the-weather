da63a8154da9edc0f4bd3147c0727c8fd6535bbd
livo <lucas.ivo@shockit.com.br>
2025-05-26 20:18:44 -0300

    refactor(auth bloc): make token required to complete tests

--- lib/UI/auth/blocs/auth_bloc/auth_bloc.dart
+++ lib/UI/auth/blocs/auth_bloc/auth_bloc.dart
@@ -38,7 +38,9 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
             token: r.token,
           ),
         );
-        localRepository.saveUserToken(token: r.token!);
+        if (r.token != null) {
+          localRepository.saveUserToken(token: r.token!);
+        }
       },
     );
   }
