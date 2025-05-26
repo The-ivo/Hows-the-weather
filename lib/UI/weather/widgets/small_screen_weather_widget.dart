edfbcf8dfa8c1c6b4a316e6cbf7b8b32c0d39bf2
livo <lucas.ivo@shockit.com.br>
2025-05-25 23:05:16 -0300

    refactor(small screen weather widget): integrate small small screen weather widget with API data

--- lib/UI/weather/widgets/small_screen_weather_widget.dart
+++ lib/UI/weather/widgets/small_screen_weather_widget.dart
@@ -9,34 +9,181 @@ class SmallScreenWeatherWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final weather = context.read<GetWeatherBloc>().state.weatherModel;
-    return Center(
+    return SingleChildScrollView(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'How\'s the weather?',
+                style: WeatherAppTypo.of(context)?.display2xsm?.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                weather!.name,
+                style: WeatherAppTypo.of(context)?.displayMd?.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Image.network(
+                  'https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Now: ${weather.main.temp}°F',
+                style: WeatherAppTypo.of(context)?.displayXsm?.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+              ),
+              SizedBox(height: 48),
+              InfoContainer(
+                title: 'Temperature details',
+                buildTempDetailsList: [
+                  BuildTempDetails(
+                    title: 'Feels like',
+                    data: '${weather.main.feelsLike.toString()}°F',
+                  ),
+                  VerticalDivider(
+                    width: 24,
+                  ),
+                  BuildTempDetails(
+                    title: 'Min temp',
+                    data: '${weather.main.tempMin.toString()}°F',
+                  ),
+                  VerticalDivider(
+                    width: 24,
+                  ),
+                  BuildTempDetails(
+                    title: 'Max temp',
+                    data: '${weather.main.tempMax.toString()}°F',
+                  )
+                ],
+              ),
+              SizedBox(height: 48),
+              InfoContainer(
+                title: 'Weather details',
+                buildTempDetailsList: [
+                  BuildTempDetails(
+                    title: 'Cloudiness',
+                    data: '${weather.clouds.all.toString()}%',
+                  ),
+                  VerticalDivider(width: 24),
+                  BuildTempDetails(
+                    title: 'Humidity',
+                    data: '${weather.main.humidity.toString()}%',
+                  ),
+                  VerticalDivider(width: 24),
+                  BuildTempDetails(
+                    title: 'Pressure',
+                    data: '${weather.main.humidity.toString()}hPa',
+                  ),
+                ],
+              ),
+              SizedBox(height: 48),
+              InfoContainer(
+                title: 'Wind details',
+                buildTempDetailsList: [
+                  BuildTempDetails(
+                    title: 'Speed',
+                    data: '${weather.wind.speed.toString()}Mph',
+                  ),
+                  VerticalDivider(width: 24),
+                  BuildTempDetails(
+                    title: 'Direction',
+                    data: '${weather.wind.deg.toString()}°',
+                  ),
+                  VerticalDivider(width: 24),
+                  BuildTempDetails(
+                    title: 'Gust',
+                    data: '${weather.wind.gust.toString()}Mph',
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BuildTempDetails extends StatelessWidget {
+  const BuildTempDetails({
+    super.key,
+    required this.title,
+    required this.data,
+  });
+
+  final String title;
+  final String data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          style: WeatherAppTypo.of(context)?.display2xsm?.copyWith(
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+              ),
+        ),
+        Text(
+          data,
+          style: WeatherAppTypo.of(context)?.display2xsm?.copyWith(
+                color: Colors.white,
+              ),
+        )
+      ],
+    );
+  }
+}
+
+class InfoContainer extends StatelessWidget {
+  const InfoContainer({
+    super.key,
+    required this.buildTempDetailsList,
+    required this.title,
+  });
+
+  final String title;
+  final List<Widget> buildTempDetailsList;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
       child: Padding(
-        padding: const EdgeInsets.all(25.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              'How\'s the weather?',
-              style: WeatherAppTypo.of(context)?.displayXsm?.copyWith(
-                    color: Colors.white,
-                  ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              weather!.name,
-              style: WeatherAppTypo.of(context)?.displaySm?.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
-                  ),
-            ),
-            SizedBox(height: 10),
-            SizedBox(height: 10),
-            Text(
-              'Now: ${weather.main.temp}°F',
+              title,
               style: WeatherAppTypo.of(context)?.display2xsm?.copyWith(
                     color: Colors.white,
-                    fontWeight: FontWeight.w700,
                   ),
+            ),
+            SizedBox(height: 8),
+            Expanded(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: buildTempDetailsList,
+              ),
             )
           ],
         ),
